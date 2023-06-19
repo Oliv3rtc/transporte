f9a5dd8f29600c8d764723c8bf7895aec7bf708d
leandro-barcelos <leandrobarcelos.f@hotmail.com>
2023-06-18 23:24:54 -0300

    Pronto

---
+++ transporte_gui.py
@@ -0,0 +1,319 @@
+#!/usr/bin/python3
+import copy
+import tkinter as tk
+import tkinter.ttk as ttk
+
+import numpy as np
+
+from trasporte import Transporte
+
+
+class App:
+    def __init__(self, master=None):
+        master.title("Problema do Transporte")
+
+        style = ttk.Style(master)
+        master.tk.call("source", "forest-light.tcl")
+        style.theme_use("forest-light")
+        master.resizable(0, 0)
+
+        self.main = ttk.Frame(master)
+        self.main.configure(height=500, width=500)
+        self.main.grid(column=0, row=0)
+        self.mainwindow = self.main
+
+        self.n = 3
+        self.m = 3
+
+        self.t = None
+        self.sbf = None
+        self.custo_total = None
+
+        self.left = None
+        self.definir_frame = None
+        self.n_spinbox = None
+        self.m_spinbox = None
+        self.definir_bt = None
+        self.dados_frame = None
+        self.custos_frame = None
+        self.custos_entries = []
+        self.ofertas_frame = None
+        self.ofertas_entries = []
+        self.demandas_frame = None
+        self.demandas_entries = []
+        self.enviar_bt = None
+        self.spinbox_frame = None
+        self.left_frame_config()
+
+        self.right_frame = None
+        self.sbf_frame = None
+        self.sbf_texts = []
+        self.metodos_frame = None
+        self.no_bt = None
+        self.minc_bt = None
+        self.vogel_bt = None
+        self.otimizar_bt = None
+        self.tmp_label = None
+        self.right_frame_config()
+
+    def left_frame_config(self):
+        self.left = ttk.Frame(self.main)
+        self.left.grid(column=0, padx=5, pady=5, row=0)
+        self.left.configure(height=200, width=200)
+
+        self.definir_frame_config()
+        self.dados_frame_config()
+
+    def definir_frame_config(self):
+        self.definir_frame = ttk.Labelframe(self.left)
+        self.definir_frame.configure(height=200, text="Defir tamanho", width=200)
+        self.definir_frame.pack()
+
+        self.spinbox_frame = ttk.Frame(self.definir_frame)
+        self.spinbox_frame.configure(height=200, width=200)
+        self.spinbox_frame.grid(column=0, row=1)
+
+        self.n_spinbox = ttk.Spinbox(self.spinbox_frame)
+        self.n_spinbox.insert(0, str(self.n))
+        self.n_spinbox.configure(from_=2, to=20, width=5)
+        self.n_spinbox.grid(column=0, padx=5, pady=5, row=0)
+
+        self.m_spinbox = ttk.Spinbox(self.spinbox_frame)
+        self.m_spinbox.insert(0, str(self.m))
+        self.m_spinbox.configure(from_=2, to=20, width=5)
+        self.m_spinbox.grid(column=1, padx=5, pady=5, row=0)
+
+        definir_bt_frame = ttk.Frame(self.definir_frame)
+        definir_bt_frame.configure(height=200, width=200)
+        definir_bt_frame.grid(column=0, row=2)
+
+        self.definir_bt = ttk.Button(definir_bt_frame, command=self.definir_tamanho)
+        self.definir_bt.configure(text="Definir", width=15)
+        self.definir_bt.pack(pady=5, side="top")
+
+    def dados_frame_config(self):
+        self.dados_frame = ttk.Labelframe(self.left)
+        self.dados_frame.configure(height=200, text="Inserir Dados", width=200)
+        self.dados_frame.pack()
+
+        self.custos_frame_config()
+
+        self.ofertas_frame_config()
+
+        self.demandas_frame_config()
+
+        self.gerar_entries()
+
+        dados_bt_frame = ttk.Frame(self.dados_frame)
+        dados_bt_frame.configure(height=200, width=200)
+        dados_bt_frame.grid(column=0, columnspan=2, padx=5, pady=5, row=2)
+
+        self.enviar_bt = ttk.Button(dados_bt_frame, command=self.ler_entries)
+        self.enviar_bt.configure(text="Definir", width=15)
+        self.enviar_bt.grid(column=0, row=0, padx=5, pady=5)
+
+        self.clear_bt = ttk.Button(dados_bt_frame, command=self.clear_left_side)
+        self.clear_bt.configure(text="Limpar", width=15)
+        self.clear_bt.grid(column=1, row=0, padx=5, pady=5)
+
+    def custos_frame_config(self):
+        self.custos_frame = ttk.Frame(self.dados_frame)
+        self.custos_frame.configure(height=200, width=200)
+        self.custos_frame.grid(column=0, pady=5, row=0)
+
+    def ofertas_frame_config(self):
+        self.ofertas_frame = ttk.Frame(self.dados_frame)
+        self.ofertas_frame.grid(column=1, pady=5, padx=5, row=0)
+
+    def demandas_frame_config(self):
+        self.demandas_frame = ttk.Frame(self.dados_frame)
+        self.demandas_frame.configure(height=200, width=200)
+        self.demandas_frame.grid(column=0, padx=5, pady=5, row=1)
+
+    def gerar_entries(self):
+        self.custos_entries = []
+        self.ofertas_entries = []
+        self.demandas_entries = []
+
+        for i in range(self.n):
+            for j in range(self.m):
+                tmp = ttk.Entry(self.custos_frame)
+                tmp.configure(width=5)
+                tmp.grid(column=j, row=i)
+                self.custos_entries.append(tmp)
+
+        for i in range(self.n):
+            tmp = ttk.Entry(self.ofertas_frame)
+            tmp.configure(width=5)
+            tmp.pack()
+            self.ofertas_entries.append(tmp)
+
+        for i in range(self.m):
+            tmp = ttk.Entry(self.demandas_frame)
+            tmp.configure(width=5)
+            tmp.grid(column=i, row=0)
+            self.demandas_entries.append(tmp)
+
+    def clear_left_side(self):
+        for entry in self.custos_entries:
+            entry.destroy()
+        self.custos_entries.clear()
+
+        for entry in self.ofertas_entries:
+            entry.destroy()
+        self.ofertas_entries.clear()
+
+        for entry in self.demandas_entries:
+            entry.destroy()
+        self.demandas_entries.clear()
+
+        self.gerar_entries()
+
+    def definir_tamanho(self):
+        self.n = int(self.n_spinbox.get())
+        self.m = int(self.m_spinbox.get())
+
+        self.n_spinbox = ttk.Spinbox(self.spinbox_frame)
+        self.n_spinbox.insert(0, str(self.n))
+        self.n_spinbox.configure(from_=2, to=20, width=5)
+        self.n_spinbox.grid(column=0, padx=5, pady=5, row=0)
+
+        self.m_spinbox = ttk.Spinbox(self.spinbox_frame)
+        self.m_spinbox.insert(0, str(self.m))
+        self.m_spinbox.configure(from_=2, to=20, width=5)
+        self.m_spinbox.grid(column=1, padx=5, pady=5, row=0)
+
+        self.clear_left_side()
+        self.clear_right_side()
+
+    def ler_entries(self):
+        ofertas = []
+        demandas = []
+        custos = []
+
+        for i in range(self.n):
+            tmp = []
+            for j in range(self.m):
+                tmp.append(int(self.custos_entries[i * self.m + j].get()))
+            custos.append(tmp)
+
+        for i in range(self.n):
+            ofertas.append(int(self.ofertas_entries[i].get()))
+
+        for j in range(self.m):
+            demandas.append(int(self.demandas_entries[j].get()))
+
+        self.t = Transporte(custos, ofertas, demandas)
+
+        self.clear_right_side()
+
+    def clear_right_side(self):
+        for label in self.sbf_texts:
+            label.destroy()
+        self.sbf_texts.clear()
+
+        self.gerar_label_sbf()
+
+    def right_frame_config(self):
+        self.right_frame = ttk.Labelframe(self.main)
+        self.right_frame.configure(height=200, text="Resultado", width=200)
+        self.right_frame.grid(column=1, padx=5, pady=5, row=0, sticky="nw")
+
+        self.sbf_frame_config()
+
+        self.metodos_frame = ttk.Frame(self.right_frame)
+        self.metodos_frame.configure(height=200, width=200)
+        self.metodos_frame.grid(column=0, padx=5, pady=5, row=2)
+
+        self.no_bt = ttk.Button(self.metodos_frame, command=self.canto_no)
+        self.no_bt.configure(text="Canto Noroeste")
+        self.no_bt.grid(column=0, row=0, padx=5, pady=5)
+
+        self.minc_bt = ttk.Button(self.metodos_frame, command=self.minimo_custo)
+        self.minc_bt.configure(text="Minimo dos Custos")
+        self.minc_bt.grid(column=1, row=0, padx=5, pady=5)
+
+        self.vogel_bt = ttk.Button(self.metodos_frame, command=self.vogel)
+        self.vogel_bt.configure(text="Vogel")
+        self.vogel_bt.grid(column=2, row=0, padx=5, pady=5)
+
+        self.otimizar_bt = ttk.Button(self.right_frame, command=self.otimizar)
+        self.otimizar_bt.configure(text="Otimizar")
+        self.otimizar_bt.grid(column=0, padx=5, pady=5, row=3)
+
+    def sbf_frame_config(self):
+        self.sbf_frame = ttk.Frame(self.right_frame)
+        self.sbf_frame.configure(height=200, width=200)
+        self.sbf_frame.grid(column=0, padx=5, pady=5, row=1)
+
+        self.gerar_label_sbf()
+
+    def gerar_label_sbf(self):
+        if self.tmp_label != None:
+            self.tmp_label.destroy()
+
+        mensagem = "Defina o problema ao lado"
+
+        if self.t != None:
+            mensagem = "Escolha um método abaixo"
+
+        if self.sbf == None:
+            self.tmp_label = tk.Label(self.sbf_frame, text=mensagem)
+            self.tmp_label.grid(row=0, column=0, sticky="nesw")
+            return
+
+        for i in range(self.n):
+            for j in range(self.m):
+                tmp = tk.Label(
+                    self.sbf_frame,
+                    text="0" if np.isnan(self.sbf[i][j]) else str(self.sbf[i][j]),
+                )
+                tmp.configure(height=2, width=6)
+                tmp.grid(column=j, row=i, sticky="nw")
+                self.sbf_texts.append(tmp)
+
+        self.custo_total_label = ttk.Label(
+            self.right_frame, text=f"Custo = {self.custo_total}"
+        )
+        self.custo_total_label.grid(row=0, column=0, padx=5, pady=5)
+
+    def canto_no(self):
+        if self.t == None:
+            return
+
+        self.sbf, self.custo_total = self.t.canto_noroeste()
+        self.n = len(self.sbf)
+        self.m = len(self.sbf[0])
+        self.clear_right_side()
+
+    def minimo_custo(self):
+        if self.t == None:
+            return
+        self.sbf, self.custo_total = self.t.minimo_dos_custos()
+        self.n = len(self.sbf)
+        self.m = len(self.sbf[0])
+        self.clear_right_side()
+
+    def vogel(self):
+        if self.t == None:
+            return
+        self.sbf, self.custo_total = self.t.vogel()
+        self.n = len(self.sbf)
+        self.m = len(self.sbf[0])
+        self.clear_right_side()
+
+    def otimizar(self):
+        if self.t == None or self.sbf == None:
+            return
+
+        self.custo_total = self.t.obter_otimo(self.sbf)
+        self.clear_right_side()
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    app.run()
---
+++ trasporte.py
@@ -0,0 +1,327 @@
+from collections import deque
+import numpy as np
+from networkx import DiGraph, find_cycle
+import copy
+
+
+class Transporte:
+    def __init__(
+        self, custos: list[list[float]], oferta: list[float], demanda: list[float]
+    ):
+        self.custos = copy.deepcopy(custos)
+        self.oferta = oferta[:]
+        self.demanda = demanda[:]
+
+        dif_oferta_demanda = sum(self.oferta) - sum(self.demanda)
+
+        if dif_oferta_demanda < 0:
+            self.oferta.append(abs(dif_oferta_demanda))
+            self.custos.append([0] * len(self.demanda))
+        elif dif_oferta_demanda > 0:
+            self.demanda.append(abs(dif_oferta_demanda))
+            for linha in self.custos:
+                linha.append(0)
+
+    def canto_noroeste(self):
+        bfs = [
+            [np.nan for _ in range(len(self.custos[0]))]
+            for _ in range(len(self.custos))
+        ]
+        custo_total = 0
+        i = 0
+        j = 0
+
+        oferta = self.oferta[:]
+        demanda = self.demanda[:]
+
+        while i < len(self.custos) and j < len(self.custos[0]):
+            max_disponivel = min(oferta[i], demanda[j])
+
+            bfs[i][j] = max_disponivel
+            custo_total += max_disponivel * self.custos[i][j]
+            oferta[i] -= max_disponivel
+            demanda[j] -= max_disponivel
+
+            if oferta[i] == 0:
+                i += 1
+            elif demanda[j] == 0:
+                j += 1
+
+        return bfs, custo_total
+
+    def minimo_dos_custos(self):
+        bfs = [
+            [np.nan for _ in range(len(self.custos[0]))]
+            for _ in range(len(self.custos))
+        ]
+        custo_total = 0
+
+        oferta = self.oferta[:]
+        demanda = self.demanda[:]
+        custos = copy.deepcopy(self.custos)
+
+        while max(oferta) != 0 or max(demanda) != 0:
+            i, j = np.argwhere(custos == np.min(custos))[0]
+
+            max_disponivel = min(oferta[i], demanda[j])
+
+            bfs[i][j] = max_disponivel
+            custo_total += max_disponivel * self.custos[i][j]
+            oferta[i] -= max_disponivel
+            demanda[j] -= max_disponivel
+
+            if oferta[i] == 0:
+                custos[i] = [np.inf for _ in range(len(custos[i]))]
+            elif demanda[j] == 0:
+                for linha in custos:
+                    linha[j] = np.inf
+
+        return bfs, custo_total
+
+    def vogel(self):
+        bfs = [
+            [np.nan for _ in range(len(self.custos[0]))]
+            for _ in range(len(self.custos))
+        ]
+        custo_total = 0
+
+        oferta = self.oferta[:]
+        demanda = self.demanda[:]
+        custos = copy.deepcopy(self.custos)
+
+        while max(oferta) != 0 or max(demanda) != 0:
+            dif_linhas, dif_colunas = dif_minimos(custos)
+
+            max_dif_linhas = np.max(dif_linhas)
+            max_dif_colunas = np.max(dif_colunas)
+
+            i = -1
+            j = -1
+
+            if max_dif_linhas >= max_dif_colunas:
+                i = np.argwhere(dif_linhas == max_dif_linhas)[0][0]
+                j = np.argwhere(custos[i] == np.min(custos[i]))[0][0]
+            else:
+                j = np.argwhere(dif_colunas == max_dif_colunas)[0][0]
+                col = [x[j] for x in custos]
+                i = np.argwhere(col == np.min(col))[0][0]
+
+            max_disponivel = min(oferta[i], demanda[j])
+
+            bfs[i][j] = max_disponivel
+            custo_total += max_disponivel * self.custos[i][j]
+            oferta[i] -= max_disponivel
+            demanda[j] -= max_disponivel
+
+            if oferta[i] == 0:
+                custos[i] = [np.inf for _ in range(len(custos[i]))]
+            elif demanda[j] == 0:
+                for dif_linhas in custos:
+                    dif_linhas[j] = np.inf
+
+        return bfs, custo_total
+
+    def obter_dual(self, sbf):
+        u = [np.nan for _ in self.oferta]
+        v = [np.nan for _ in self.demanda]
+
+        index_basicas = list(np.argwhere(np.asarray(sbf) >= 0))
+        u[index_basicas[0][0]] = 0
+
+        while any(np.isnan(u)) or any(np.isnan(v)):
+            for i, j in index_basicas:
+                if np.isnan(u[i]) and not np.isnan(v[j]):
+                    u[i] = self.custos[i][j] - v[j]
+                elif not np.isnan(u[i]) and np.isnan(v[j]):
+                    v[j] = self.custos[i][j] - u[i]
+                else:
+                    continue
+
+        return u, v
+
+    def obter_otimo(self, sbf):
+        while True:
+            n_basicas = (len(sbf) * len(sbf[0])) - np.count_nonzero(np.isnan(sbf))
+
+            if n_basicas != len(self.demanda) + len(self.oferta) - 1:
+                for i in range(len(self.oferta)):
+                    for j in range(len(self.demanda)):
+                        coluna = [x[j] for x in sbf]
+                        if np.count_nonzero(np.isnan(sbf[i])) == 1 and np.count_nonzero(
+                            np.isnan(coluna)
+                        ):
+                            x, y = np.argwhere(coluna == np.nan)[0]
+                            sbf[x][y] = 0
+
+            u, v = self.obter_dual(sbf)
+
+            custos_reduzidos = [[0 for _ in self.demanda] for _ in self.oferta]
+
+            for i in range(len(self.oferta)):
+                for j in range(len(self.demanda)):
+                    custos_reduzidos[i][j] = self.custos[i][j] - u[i] - v[j]
+
+            min_custo = np.min(custos_reduzidos)
+            if min_custo >= 0:
+                break
+
+            i, j = np.argwhere(custos_reduzidos == min_custo)[0]
+            sbf[i][j] = np.inf
+
+            ciclo = encontrar_ciclo(sbf, i, j)
+
+            sub = [ciclo[x] for x in range(len(ciclo)) if x % 2 != 0]
+            sai = min([sbf[x][y] for x, y in sub])
+            sbf[ciclo[0][0]][ciclo[0][1]] = sai
+
+            ciclo.pop(0)
+            ciclo.pop()
+            t = -1
+
+            for i, j in ciclo:
+                if sbf[i][j] == sai and t < 0:
+                    sbf[i][j] = np.nan
+                else:
+                    sbf[i][j] += sai * t
+                t = -t
+
+        return np.nansum(np.asarray([sbf]) * np.asanyarray(self.custos))
+
+
+def encontrar_ciclo(matriz, i_inicial, j_inicial):
+    g = DiGraph()
+    fila = deque()
+
+    fila.append(((i_inicial, j_inicial), False))
+
+    while len(fila) > 0:
+        no = fila.popleft()
+
+        if no[1] == False:
+            j = no[0][1]
+            coluna = [linha[j] for linha in matriz]
+            for i in range(len(coluna)):
+                if i == no[0][0] or g.has_node((i, j)) or np.isnan(coluna[i]):
+                    continue
+
+                g.add_edge(no[0], (i, j))
+                fila.append(((i, j), not no[1]))
+        elif no[1] == True:
+            i = no[0][0]
+            linha = matriz[i]
+            for j in range(len(linha)):
+                if (i, j) == (i_inicial, j_inicial):
+                    g.add_edge(no[0], (i, j))
+                    break
+
+                if j == no[0][1] or g.has_node((i, j)) or np.isnan(linha[j]):
+                    continue
+
+                g.add_edge(no[0], (i, j))
+                fila.append(((i, j), not no[1]))
+
+    ciclo = find_cycle(g, (i_inicial, j_inicial))
+    return [(i_inicial, j_inicial)] + [n[1] for n in ciclo]
+
+
+def dif_minimos(matriz):
+    dif_colunas = []
+    dif_linhas = []
+
+    dif_linhas = [sorted(linha)[:2] for linha in matriz]
+    for i, d in enumerate(dif_linhas):
+        if d[1] != np.inf:
+            dif_linhas[i] = d[1] - d[0]
+        else:
+            dif_linhas[i] = -min(d)
+
+    dif_colunas = [sorted(coluna)[:2] for coluna in zip(*matriz)]
+    for i, d in enumerate(dif_colunas):
+        if d[1] != np.inf:
+            dif_colunas[i] = d[1] - d[0]
+        else:
+            dif_colunas[i] = -min(d)
+
+    return [dif_linhas, dif_colunas]
+
+
+def min_matriz(matriz, linhas_ignoradas, colunas_ignoradas):
+    valor_min = np.inf
+    pos_min = (0, 0)
+
+    for i in range(0, len(matriz)):
+        if i in linhas_ignoradas:
+            continue
+        for j in range(0, len(matriz[i])):
+            if j in colunas_ignoradas:
+                continue
+
+            if matriz[i][j] < valor_min:
+                valor_min = matriz[i][j]
+                pos_min = (i, j)
+
+    return pos_min
+
+
+def get_input(prompt):
+    while True:
+        try:
+            user_input = input(prompt).split(" ")
+            return user_input
+        except ValueError:
+            print("Entrada inválida. Tente novamente.")
+
+
+def main():
+    print("Programa de Resolução do Problema do Transporte")
+
+    t = Transporte(
+        [[2, 3, 4, 5], [3, 2, 5, 2], [4, 1, 2, 3]],
+        [15, 20, 25],
+        [8, 10, 12, 15],
+    )
+
+    menu = 1
+    while True:
+        if menu == 1:
+            print("\nOpções:")
+            print("1. Método do Canto Noroeste")
+            print("2. Método do Mínimo dos Custos")
+            print("3. Método de Vogel")
+            print("0. Sair")
+
+            choice = int(get_input("Digite o número da opção desejada: ")[0])
+
+            if choice == 1:
+                sbf, custo = t.canto_noroeste()
+                print(sbf)
+                print(custo)
+                custo = t.obter_otimo(sbf)
+                print(sbf)
+                print(custo)
+                # menu = 2
+            elif choice == 2:
+                sbf, custo = t.minimo_dos_custos()
+                print(sbf)
+                print(custo)
+                custo = t.obter_otimo(sbf)
+                print(sbf)
+                print(custo)
+                # menu = 2
+            elif choice == 3:
+                sbf, custo = t.vogel()
+                print(sbf)
+                print(custo)
+                custo = t.obter_otimo(sbf)
+                print(sbf)
+                print(custo)
+            #     menu = 2
+            elif choice == 0:
+                print("Programa encerrado.")
+                break
+            else:
+                print("Opção inválida. Tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
